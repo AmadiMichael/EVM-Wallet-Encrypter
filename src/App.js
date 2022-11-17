6bd4d9c4161db08b861f1af10f6efc722d7943f2
Michael Amadi <97467680+AmadiMichael@users.noreply.github.com>
2022-11-17 14:44:08 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,228 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { ethers } from "ethers";
+import { Loader, Center } from "@mantine/core";
 
 function App() {
+  const [currentWalletInfo, setCurrentWalletInfo] = useState({
+    walletAddress: "",
+    publicKey: "",
+    privateKey: "",
+    mnemonicPhrase: {},
+    encryptedWallet: {},
+  });
+  const [isEncrypting, setIsEncrypting] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [encryptionProgress, setEncryptionProgress] = useState(null);
+
+  async function encrypter(walletInstance, password) {
+    try {
+      function callback(progress) {
+        setEncryptionProgress(
+          "Encrypting: " + parseInt(progress * 100) + "% complete"
+        );
+      }
+
+      const encryptedWallet = await walletInstance.encrypt(password, callback);
+
+      setCurrentWalletInfo({
+        walletAddress: walletInstance.address,
+        publicKey: walletInstance.publicKey,
+        privateKey: walletInstance.privateKey,
+        mnemonicPhrase: walletInstance.mnemonic ?? { phrase: "" },
+        encryptedWallet: encryptedWallet,
+      });
+    } catch (err) {
+      setIsEncrypting(false);
+      setErrorMessage(err.message);
+    }
+  }
+
+  async function encryptNew(event) {
+    event.preventDefault();
+    setErrorMessage(null);
+    setCurrentWalletInfo({
+      walletAddress: "",
+      publicKey: "",
+      privateKey: "",
+      mnemonicPhrase: {},
+      encryptedWallet: {},
+    });
+    setIsEncrypting(true);
+
+    let wallet;
+    try {
+      wallet = ethers.Wallet.createRandom();
+      await encrypter(wallet, event.target.pass.value);
+    } catch (err) {
+      setErrorMessage(err.message);
+    }
+
+    setIsEncrypting(false);
+  }
+
+  async function encryptPK(event) {
+    event.preventDefault();
+
+    setErrorMessage(null);
+
+    setCurrentWalletInfo({
+      walletAddress: "",
+      publicKey: "",
+      privateKey: "",
+      mnemonicPhrase: {},
+      encryptedWallet: {},
+    });
+    setIsEncrypting(true);
+
+    let wallet;
+
+    try {
+      wallet = new ethers.Wallet(event.target.pk.value);
+      console.log(wallet);
+      await encrypter(wallet, event.target.pass.value);
+    } catch (err) {
+      setErrorMessage(err.message);
+    }
+
+    setIsEncrypting(false);
+  }
+
+  async function encryptMnemonic(event) {
+    event.preventDefault();
+    setErrorMessage(null);
+    setCurrentWalletInfo({
+      walletAddress: "",
+      publicKey: "",
+      privateKey: "",
+      mnemonicPhrase: {},
+      encryptedWallet: {},
+    });
+
+    setIsEncrypting(true);
+
+    if (ethers.utils.isValidMnemonic(event.target.mnemonic.value, "en")) {
+      let wallet;
+
+      try {
+        wallet = ethers.Wallet.fromMnemonic(event.target.mnemonic.value);
+        await encrypter(wallet, event.target.pass.value);
+      } catch (err) {
+        setErrorMessage(err.message);
+      }
+    } else {
+      setErrorMessage("Invalid mnemonic phrase");
+    }
+
+    setIsEncrypting(false);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h4> GENERATE A NEW WALLET AND ENCRYPT IT</h4>
+      <form onSubmit={encryptNew}>
+        <input id="pass" placeholder="Password" required /> <p />
+        <button type="submit"> ENCRYPT </button>
+      </form>
+      <h4>
+        {" "}
+        ENCRYPT A WALLET VIA PRIVATE KEY <p />{" "}
+        <h6 style={{ color: "grey" }}>
+          {" "}
+          (Wallets generated using a private key do not have a mnemonic phrase){" "}
+        </h6>{" "}
+      </h4>
+      <form onSubmit={encryptPK}>
+        <input id="pk" placeholder="Private Key" required /> <p />
+        <input id="pass" placeholder="Password" required /> <p />
+        <button type="submit"> ENCRYPT </button>
+      </form>
+      <h4> ENCRYPT A WALLET VIA MNEMONIC PHRASE </h4>
+      <form onSubmit={encryptMnemonic}>
+        <input id="mnemonic" placeholder="Mnemonic phrase" required /> <p />
+        <input id="pass" placeholder="Password" required /> <p />
+        <button type="submit"> ENCRYPT </button>
+      </form>
+
+      {isEncrypting ? (
+        <div
+          className="box"
+          style={{
+            marginTop: "2em",
+            whiteSpace: "normal",
+            wordWrap: "break-word",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <Center>
+            <Loader mt={15} color="black" />
+          </Center>
+
+          <h3>{encryptionProgress} </h3>
+        </div>
+      ) : null}
+      <p />
+      {errorMessage ? (
+        <div
+          className="box"
+          style={{
+            marginTop: "2em",
+            whiteSpace: "normal",
+            wordWrap: "break-word",
+          }}
+        >
+          {errorMessage}
+        </div>
+      ) : null}
+      <p />
+
+      {currentWalletInfo.walletAddress !== "" ? (
+        <div
+          className="box"
+          style={{
+            padding: "3em",
+            whiteSpace: "normal",
+            wordWrap: "break-word",
+          }}
+        >
+          <h4> WALLET ADDRESS </h4> {currentWalletInfo.walletAddress}{" "}
+          <p
+            style={{
+              marginBottom: "3em",
+            }}
+          />
+          <h4> PUBLIC KEY </h4> {currentWalletInfo.publicKey}{" "}
+          <p
+            style={{
+              marginBottom: "3em",
+            }}
+          />
+          <h4> PRIVATE KEY </h4> {currentWalletInfo.privateKey}
+          <p
+            style={{
+              marginBottom: "3em",
+            }}
+          />
+          <h4>
+            {" "}
+            MNEMONIC PHRASE <p />{" "}
+            <h6 style={{ color: "grey" }}>
+              {" "}
+              (Wallets generated using a private key do not have a mnemonic
+              phrase){" "}
+            </h6>{" "}
+          </h4>
+          {currentWalletInfo.mnemonicPhrase !== {}
+            ? currentWalletInfo.mnemonicPhrase.phrase
+            : null}{" "}
+          <p
+            style={{
+              marginBottom: "3em",
+            }}
+          />
+          <h4> ENCRYPTED WALLET </h4>
+          {currentWalletInfo.encryptedWallet.toString()}
+        </div>
+      ) : null}
     </div>
   );
 }
