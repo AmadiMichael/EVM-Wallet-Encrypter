c5df5d38bd99cdcd89b0f97e386cf8523777b0c3
AmadiMichael <amadimichaeld@gmail.com>
2022-12-05 22:28:52 +0100

    Improved UI, added popup notifications

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import "./App.css";
 import { useState } from "react";
 import { ethers } from "ethers";
 import { Loader, Center } from "@mantine/core";
-
+import {
+  NotificationContainer,
+  NotificationManager,
+} from "react-notifications";
+import "react-notifications/lib/notifications.css";
 function App() {
+  const r = document.querySelector(":root");
   const [currentWalletInfo, setCurrentWalletInfo] = useState({
     walletAddress: "",
     publicKey: "",
@@ -38,8 +43,27 @@ function App() {
     }
   }
 
+  async function isOnline() {
+    console.log(window.navigator.onLine);
+    return window.navigator.onLine;
+  }
+
   async function encryptNew(event) {
     event.preventDefault();
+
+    if (await isOnline()) {
+      NotificationManager.error(
+        "For full security, kindly turn off all internet connection"
+      );
+      return;
+    }
+    if (isEncrypting) {
+      NotificationManager.warning(
+        `Currently encrypting a wallet.\nPlease wait`
+      );
+      return;
+    }
+
     setErrorMessage(null);
     setCurrentWalletInfo({
       walletAddress: "",
@@ -63,7 +87,18 @@ function App() {
 
   async function encryptPK(event) {
     event.preventDefault();
-
+    if (await isOnline()) {
+      NotificationManager.error(
+        "For full security, kindly turn off all internet connection"
+      );
+      return;
+    }
+    if (isEncrypting) {
+      NotificationManager.warning(
+        `Currently encrypting a wallet.\nPlease wait`
+      );
+      return;
+    }
     setErrorMessage(null);
 
     setCurrentWalletInfo({
@@ -90,6 +125,18 @@ function App() {
 
   async function encryptMnemonic(event) {
     event.preventDefault();
+    if (await isOnline()) {
+      NotificationManager.error(
+        "For full security, kindly turn off all internet connection"
+      );
+      return;
+    }
+    if (isEncrypting) {
+      NotificationManager.warning(
+        `Currently encrypting a wallet.\nPlease wait`
+      );
+      return;
+    }
     setErrorMessage(null);
     setCurrentWalletInfo({
       walletAddress: "",
@@ -117,73 +164,138 @@ function App() {
     setIsEncrypting(false);
   }
 
+  function displays(val) {
+    if (val == "nw") {
+      r.style.setProperty("--nw", "block");
+      r.style.setProperty("--pk", "none");
+      r.style.setProperty("--mp", "none");
+    } else if (val == "pk") {
+      r.style.setProperty("--nw", "none");
+      r.style.setProperty("--pk", "block");
+      r.style.setProperty("--mp", "none");
+    } else if (val == "mp") {
+      r.style.setProperty("--nw", "none");
+      r.style.setProperty("--pk", "none");
+      r.style.setProperty("--mp", "block");
+    }
+  }
+
   return (
     <div className="App">
       <header>
         <h1>WALLET ENCRYPTER</h1>
+        <NotificationContainer />
         <div className="line" />
       </header>
+
+      <h3> ENCRYPT ANY EVM WALLET OFFLINE WITH A PASSWORD INTO A JSON FILE </h3>
+
       <p
         style={{
           marginBottom: "4em",
         }}
       />
-      <h4> GENERATE A NEW WALLET AND ENCRYPT IT</h4>
-      <p />
-      <h6
-        style={{
-          color: "grey",
-        }}
-      >
-        {" "}
-        Info: Create a new wallet and encrypt it with a password of your choice{" "}
-      </h6>
-      <form onSubmit={encryptNew}>
-        <input id="pass" placeholder="Password" required /> <p />
-        <button type="submit"> ENCRYPT </button>
-      </form>
+      <ul id="ul-nb" style={{ display: "flex", justifyContent: "center" }}>
+        <li
+          onClick={() => {
+            displays("nw");
+          }}
+        >
+          <div>Create New Wallet and Encrypt</div>
+        </li>
+        <li
+          onClick={() => {
+            displays("pk");
+          }}
+        >
+          <div>Encrypt a wallet via Private keys</div>
+        </li>
+        <li
+          onClick={() => {
+            displays("mp");
+          }}
+        >
+          <div>Encrypt a wallet via Mnemonic phrase</div>
+        </li>
+      </ul>
       <p
         style={{
-          marginBottom: "3em",
+          marginBottom: "6.3em",
         }}
       />
-      <h4>
-        {" "}
-        ENCRYPT A WALLET VIA PRIVATE KEY <p />{" "}
-      </h4>
-      <h6
-        style={{
-          color: "grey",
-        }}
-      >
-        {" "}
-        Info: Encrypt your private key with a password of your choice. <br />{" "}
-        (Wallets generated using a private key do not have a mnemonic phrase)
-      </h6>
-      <form onSubmit={encryptPK}>
-        <input id="pk" placeholder="Private Key" required /> <p />
-        <input id="pass" placeholder="Password" required /> <p />
-        <button type="submit"> ENCRYPT </button>
-      </form>{" "}
-      <p
-        style={{
-          marginBottom: "3em",
-        }}
-      />
-      <h4> ENCRYPT A WALLET VIA MNEMONIC PHRASE </h4>
-      <h6
-        style={{
-          color: "grey",
-        }}
-      >
-        {" "}
-        Info: Encrypt your Mnemonic phrase with a password of your choice{" "}
-      </h6>
-      <form onSubmit={encryptMnemonic}>
-        <input id="mnemonic" placeholder="Mnemonic phrase" required /> <p />
-        <input id="pass" placeholder="Password" required /> <p />
-        <button type="submit"> ENCRYPT </button>
-      </form>
+
+      <div className="nw">
+        <h4> GENERATE A NEW WALLET AND ENCRYPT IT</h4>
+        <p />
+        <h6
+          style={{
+            color: "grey",
+          }}
+        >
+          {" "}
+          Info: Create a new wallet and encrypt it with a password of your
+          choice{" "}
+        </h6>
+        <form onSubmit={encryptNew}>
+          <input id="pass" placeholder="Password" required /> <p />
+          <button type="submit"> ENCRYPT </button>
+        </form>
+        <p
+          style={{
+            marginBottom: "3em",
+          }}
+        />
+      </div>
+
+      <div className="pk">
+        <h4>
+          {" "}
+          ENCRYPT A WALLET VIA PRIVATE KEY <p />{" "}
+        </h4>
+        <h6
+          style={{
+            color: "grey",
+          }}
+        >
+          {" "}
+          Info: Encrypt your private key with a password of your choice. <br />{" "}
+          (Wallets generated using a private key do not have a mnemonic phrase)
+        </h6>
+        <form onSubmit={encryptPK}>
+          <input id="pk" placeholder="Private Key" required /> <p />
+          <input id="pass" placeholder="Password" required /> <p />
+          <button type="submit" disabled={isEncrypting}>
+            {" "}
+            ENCRYPT{" "}
+          </button>
+        </form>{" "}
+        <p
+          style={{
+            marginBottom: "3em",
+          }}
+        />
+      </div>
+
+      <div className="mp">
+        <h4> ENCRYPT A WALLET VIA MNEMONIC PHRASE </h4>
+        <h6
+          style={{
+            color: "grey",
+          }}
+        >
+          {" "}
+          Info: Encrypt your Mnemonic phrase with a password of your choice{" "}
+        </h6>
+        <form onSubmit={encryptMnemonic}>
+          <input id="mnemonic" placeholder="Mnemonic phrase" required /> <p />
+          <input id="pass" placeholder="Password" required /> <p />
+          <button type="submit" disabled={isEncrypting}>
+            {" "}
+            ENCRYPT{" "}
+          </button>
+        </form>
+      </div>
+
       {isEncrypting ? (
         <div
           className="box"
